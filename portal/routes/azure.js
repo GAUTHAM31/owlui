9c06ecf351d6d6f38eea9fdcd46d58a11623e542
Ubuntu <ubuntu@ip-172-31-28-2.ap-south-1.compute.internal>
2018-08-02 12:39:59 +0000

    added db module

---
+++ portal/routes/azure.js
@@ -0,0 +1,42 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var config = require('./db.js')
+
+// Create connection to database
+
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) 
+   {
+     if (err) 
+       {
+          console.log(err)
+       }
+    else
+       {
+           queryDatabase()
+       }
+   }
+ );
+
+function queryDatabase()
+   { console.log('Reading rows from the Table...');
+
+       // Read all rows from table
+     request = new Request(
+          "SELECT MRNO from Transaction_Record",
+             function(err, rowCount, rows) 
+                {
+                    console.log(rowCount + ' row(s) returned');
+                    process.exit();
+                }
+            );
+
+     request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+         });
+             });
+     connection.execSql(request);
+   }
