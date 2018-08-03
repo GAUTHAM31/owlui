a825d0ed9295df75588e6b184599903c6c2c01bf
GAUTHAM31 <gauthamgajith@yahoo.co.in>
2018-08-03 14:45:30 +0000

    Added ajax querying

--- webportal/routes/mrquery.js
+++ webportal/routes/mrquery.js
@@ -1,10 +1,58 @@
 var express = require('express');
 var router = express.Router();
+//For Database
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var config = require('./db.js')
+
+
+
+function queryDatabase(res,connection)
+   { console.log('Reading rows from the Table...');
+
+       // Read all rows from table
+       var result = {};
+     request = new Request(
+          "SELECT MRNO from Transaction_Record",
+             function(err, rowCount, rows) 
+                {
+                    console.log(rowCount + ' row(s) returned');
+                    console.log(result);
+                    res.send(result);
+                }
+            );
+
+     request.on('row', function(columns) {
+        columns.forEach(function(column) {
+          result[column.value] = {};
+         });
+             });
+     connection.execSql(request);
+     
+     //res.send(result);
+   }
 
 router.post('/', function(req, res, next) {
  	var obj = {};
-	console.log('body: ' + JSON.stringify(req.body));
-	res.send(req.body);
+ 	console.log('Function');
+ 	var connection = new Connection(config);
+ 	connection.on('connect', function(err) 
+   {
+     if (err) 
+       {
+          console.log(err);
+       }
+    else
+       {
+       		console.log('Connected');
+        	queryDatabase(res,connection);
+       }
+   }
+ );
+	
+	//res.send(req.body);
 });
+
+
 
 module.exports = router;
